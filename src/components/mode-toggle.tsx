5eb9b0ae27ac4ca78097920890a5414189d93f31
Anupam <itsmrad@gmail.com>
2026-01-22 00:09:50 +0530

    chore: Update dependencies, refactor UI components, and refresh content and assets across the portfolio.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export function ModeToggle({ className }: { className?: string }) {
-  const { theme, setTheme } = useTheme();
+	const { theme, setTheme } = useTheme();
 
-  return (
-    <Button
-      type="button"
-      variant="link"
-      size="icon"
-      className={cn(className)}
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    >
-      <SunIcon className="h-full w-full" />
-      <MoonIcon className="hidden h-full w-full" />
-    </Button>
-  );
+	return (
+		<Button
+			type="button"
+			variant="link"
+			size="icon"
+			className={cn(className)}
+			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+		>
+			<SunIcon className="h-full w-full" />
+			<MoonIcon className="hidden h-full w-full" />
+		</Button>
+	);
 }
